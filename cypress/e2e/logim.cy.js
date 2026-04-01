041c27c60775df6f56a34812869f7d5d6aad858c
Jean Silva <jancac3@gmail.com>
2026-03-31 21:04:34 -0300

    User update scenario added

--- cypress/e2e/logim.cy.js
+++ cypress/e2e/logim.cy.js
@@ -8,18 +8,43 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: '[name="firstName"]',
+    middleNameField: '[name="middleName"]',
+    lastNameField: '[name="lastName"]',
+    genericField: '.oxd-input--active',
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: '.--close',
+    submitButton: "[type='submit']"
+    
     }
 
 
-  it('Login success', () => {
+  it.only('Login success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username) 
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('First')
+    cy.get(selectorsList.middleNameField).clear().type('middle')
+    cy.get(selectorsList.lastNameField).clear().type('last')
+    cy.get(selectorsList.genericField).eq(3).clear().type('nickname')
+    cy.get(selectorsList.genericField).eq(4).clear().type('44444')
+    cy.get(selectorsList.genericField).eq(5).clear().type('55555')
+    cy.get(selectorsList.genericField).eq(6).clear().type('66666')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2026-03-31')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericField).eq(8).clear().type('77777')
+    
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain','Successfully Updated')
   })
+  })
+  
   it('Login fail',() => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username) 
@@ -27,5 +52,4 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
     
-})
 })
