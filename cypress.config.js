aaa7815d9c9ac04409fe12282aeccd463e1cb8df
Jean Silva <jancac3@gmail.com>
2026-04-03 10:56:16 -0300

    add readme

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
-const { defineConfig } = require("cypress");
+module.exports = {
+  allowCypressEnv: false,
 
-module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
   },
-});
+};
